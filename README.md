53163a31deef90bb16f8d7bcb3c73860685bb7e7
Traian <traian@calypsoai.com>
2020-09-01 22:47:35 +0100

    Small changes and README run instructions

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # auto-booking-system
+
+Currently this is set up to automatically book a gym session for Flyefit at a given location and time. To modify this, simply replace xpaths in the `config.py` file.
+
+## Run
+`ssh`
+```shell script
+git clone git@github.com:svintit/auto-booking-system.git
+cd auto-booking-system
+python3 -m auto_booking_system
+```
+
+### or 
+
+`https`
+```shell script
+git clone https://github.com/svintit/auto-booking-system.git
+cd auto-booking-system
+python3 -m auto_booking_system
+```
--- auto_booking_system/config.py
+++ auto_booking_system/config.py
@@ -10,10 +10,16 @@ class LoginConfig(Config):
 
 class BookConfig(Config):
     location_button_xpath = "/html/body/div[1]/div[1]/div/section/div[2]/div/form/div/div[2]/div/div/div[2]/span"
+    # xpath for the location of the gym below
     location_xpath = "/html/body/div[1]/div[1]/div/section/div[2]/div/form/div/div[2]/div/div/div[3]/div/ul/li[2]"
+
     date_button_xpath = "/html/body/div[1]/div[1]/div/section/div[2]/div/form/div/div[3]/div/div/div[2]/span"
+    # date to book for (usually 24 hours in advance so set to "Tomorrow")
     date_xpath = "/html/body/div[1]/div[1]/div/section/div[2]/div/form/div/div[3]/div/div/div[3]/div/ul/li[2]"
+
+    # time to book for
     book_button_xpath = (
         "/html/body/div[1]/div[1]/div/section/div[4]/div/div[11]/a/div[3]/p[2]"
     )
+
     confirm_button_xpath = "/html/body/div[1]/div[2]/div/div/div[3]/a[1]"
